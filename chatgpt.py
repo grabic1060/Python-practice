6f82f2833e29e990d2e1f34ba4c1d6276dcbc975
grabic1060 <77672508+grabic1060@users.noreply.github.com>
2023-03-12 20:24:31 +0900

    Add files via upload

---
+++ chatgpt.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+import time
+
+root = tk.Tk()
+root.title("시계")
+
+current_time = time.strftime("%H:%M:%S")
+
+clock_label = tk.Label(root, text=current_time, font=("Helvetica", 48))
+clock_label.pack(pady=20)
+
+def update_time():
+    current_time = time.strftime("%H:%M:%S")
+    clock_label.config(text=current_time)
+    root.after(1000, update_time)
+
+update_time()
+root.mainloop()
---
+++ practice.py
@@ -0,0 +1,5 @@
+L=input()
+sum=0
+if L.find('도')==0 :
+    sum+=1
+    L.replace('도','')
