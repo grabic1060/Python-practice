f65c200f392513962afdd2671bc8a1bf30fcbfb2
Yoon <jiwookyoon0744@gmail.com>
2023-03-26 16:20:15 +0900

    2023/03/26

---
+++ practice-tk.py
@@ -0,0 +1,17 @@
+import turtle as T
+import random
+T.colormode(255)
+T.shapesize(0.05,0.05,0.05)
+T.shape('circle')
+T.bgcolor('black')
+T.color('yellow')
+T.speed(0)
+for i in range(0,50):
+    T.circle(150)
+    T.lt(360/50)
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    T.color(r,g,b)
+T.exitonclick()
+
--- practice.py
+++ practice.py
@@ -1,34 +1,16 @@
-class calculator():
-    def __init__(self,A,B):
-        self.A=A
-        self.B=B
-
-    def plus(self):
-        sum=self.A+self.B
-        return sum
-
-    def minus(self):
-        sum = self.A-self.B
-        return sum
-
-    def multiple(self):
-        sum = self.A*self.B
-        return sum
-
-    def divide(self):
-        sum = self.A/self.B
-        return sum
-
-a=float(input())
-symbol=input()
-b=float(input())
-
-result=calculator(a,b)
-if (symbol == '+'):
-    print(result.plus())
-if (symbol == '-'):
-    print(result.minus())
-if (symbol == '*'):
-    print(result.multiple())
-if (symbol == '/'):
-    print(result.divide())
+import random
+num=random.randint(1,100)
+print('시도 횟수 설정')
+attempt=int(input())
+print('범위: 1~100\n시작!')
+for i in range(0,attempt):
+    ipt=int(input())
+    if ipt < num:
+        print('Up')
+    if ipt > num:
+        print('Down')
+    if ipt == num:
+        print('성공!')
+        exit()
+    print(attempt-i-1,'회 남았습니다')
+print('실패..')
