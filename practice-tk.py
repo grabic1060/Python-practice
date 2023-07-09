7b1dd70ec332e2074cc78e54d2526c5ec4a8fccc
Yoon <jiwookyoon0744@gmail.com>
2023-07-09 12:19:08 +0900

    20230709

--- practice-tk.py
+++ practice-tk.py
@@ -1,29 +1,26 @@
-import pygame  # 파이게임 모듈 불러오기
-import sys     # 게임 시스템 종료용
-
-X = 600  # 가로 크기
-Y = 600  # 세로 크기
-pygame.init()  # pygame 초기화
-
-# 창 크기 정하기
+import pygame
+import sys
+from pygame import Rect
+X = 1000
+Y = 800
 SURFACE = pygame.display.set_mode((X, Y))
-# 창 업데이트 조정하기
-FPSCLOCK = pygame.time.Clock()
-# 창 이름 정하기
-pygame.display.set_caption("파이게임 해보기")
 
-# 게임 루프 시작
-i = 0
+player = (X/2, Y-100)
+playercolor = (0, 0, 255) # blue
+
+obstacle = Rect(X/2+50, 0, 60, 500)
+obstaclecolor = (255, 0, 0) # red
+
+plain = Rect(0, Y-30, X, 30)
+plaincolor = (0, 255, 0) # green
+
 while True:
-    for event in pygame.event.get():   # 이벤트 받기
-        if event.type == pygame.QUIT:  # 종료 이벤트(창 닫기) 라면
-            pygame.quit()              # 파이게임 종료
-            sys.exit()                 # 파이썬 종료
-    SURFACE.fill((255, 255, 255))      # 배경 채우기(흰색)
-    pygame.draw.rect(SURFACE, (255, 0, 0), (100, i, 100, 100))  # 빨간색 사각형 생성
-    pygame.draw.ellipse(SURFACE, (0, 0, 255), (i, 100, 75, 75))  # 파란색 타원 생성
-    i += 1  # 1씩 증가
-    if i > Y:  # Y좌표계를 초과하면
-        i = 0  # 0으로 초기화
-    pygame.display.update()            # 창 업데이트
-    FPSCLOCK.tick(120)  # 초당 120회로 업데이트를 조절
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    SURFACE.fill((0, 0, 0))
+    pygame.draw.circle(SURFACE, playercolor, player, 30)
+    pygame.draw.rect(SURFACE, obstaclecolor, obstacle)
+    pygame.draw.rect(SURFACE, plaincolor, plain)
+    pygame.display.update()
